41ac2c32754b7838cf7d9429e45d611b16cfff41
Ratan Varghese <ratan.varghese@gmail.com>
2017-11-15 16:55:59 -0500

    Broken implementation

--- uni-queue.lua
+++ uni-queue.lua
@@ -1,3 +1,5 @@
+otom = require("otom")
+
 local uq = {}
 
 function uq.new()
@@ -6,37 +8,83 @@ function uq.new()
 		__index = uq
 	}
 	setmetatable(res, mt)
+	res:clear()
 	return res
 end
 
-function uq:push()
+local function push_common(self, new_elem, on_top)
+	if self.idx_by_elem[new_elem] ~= nil then
+		return false
+	end
+
+	if on_top then
+		if self._len > 0 then
+			self.top = self.top + 1
+		end
+		self.elem_by_idx[self.top] = new_elem
+	else
+		if self._len > 0 then
+			self.bot = self.bot - 1
+		end
+		self.elem_by_idx[self.bot] = new_elem
+	end
+	self._len = self._len + 1
+	return true
+end
+
+function uq:push(new_elem)
+	return push_common(self, new_elem, self.top_is_right)
 end
 
 function uq:push_left()
-end
-
-function uq:extend()
-end
-
-function uq:extend_right()
+	return push_common(self, new_elem, not self.top_is_right)
 end
 
 function uq:clear()
+	local ft, rt = otom.new()
+	self.top = 0
+	self.bot = 0
+	self._len = 0
+	self.elem_by_idx = ft
+	self.idx_by_elem = rt
+	self.top_is_right = true
+end
+
+local function pop_common(self, on_top)
+	if self._len == 0 then
+		return nil
+	end
+
+	local idx = 0
+	if on_top then
+		idx = self.top
+		self.top = self.top - 1
+	else
+		idx = self.bot
+		self.bot = self.bot + 1
+	end
+	local res = self.elem_by_idx[idx]
+	self.elem_by_idx[idx] = nil
+	self._len = self._len - 1
+	return res
 end
 
 function uq:pop()
+	return pop_common(self, self.top_is_right)
 end
 
 function uq:pop_left()
+	return pop_common(self, not self.top_is_right)
 end
 
 function uq:remove()
 end
 
 function uq:reverse()
+	self.top_is_right = not self.top_is_right
 end
 
-function uq:rotate()
+function uq:rotate(count)
 end
 
 function uq:right_to_left()
@@ -46,9 +94,11 @@ function uq:left_to_right()
 end
 
 function uq:len()
+	return self._len
 end
 
-function uq:contains()
+function uq:contains(elem)
+	return self.idx_by_elem[elem] ~= nil
 end
 
 uq.pop_right = uq.pop
