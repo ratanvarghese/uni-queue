4a662d3063c29f7e427099294025ac6068f414d4
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2017-11-16 21:33:50 -0500

    Renamed variables

--- uni-queue.lua
+++ uni-queue.lua
@@ -16,41 +16,41 @@ local function push_common(self, new_elem, on_top)
 	if new_elem == nil then
 		return false
 	end
-	if self.idx_by_elem[new_elem] ~= nil then
+	if self._idx_by_elem[new_elem] ~= nil then
 		return false
 	end
 
 	if on_top then
 		if self._len > 0 then
-			self.top = self.top + 1
+			self._top = self._top + 1
 		end
-		self.elem_by_idx[self.top] = new_elem
+		self._elem_by_idx[self._top] = new_elem
 	else
 		if self._len > 0 then
-			self.bot = self.bot - 1
+			self._bot = self._bot - 1
 		end
-		self.elem_by_idx[self.bot] = new_elem
+		self._elem_by_idx[self._bot] = new_elem
 	end
 	self._len = self._len + 1
 	return true
 end
 
 function uq:push(new_elem)
-	return push_common(self, new_elem, self.top_is_right)
+	return push_common(self, new_elem, self._top_is_right)
 end
 
 function uq:push_left(new_elem)
-	return push_common(self, new_elem, not self.top_is_right)
+	return push_common(self, new_elem, not self._top_is_right)
 end
 
 function uq:clear()
 	local ft, rt = otom.new()
-	self.top = 0
-	self.bot = 0
+	self._top = 0
+	self._bot = 0
 	self._len = 0
-	self.elem_by_idx = ft
-	self.idx_by_elem = rt
-	self.top_is_right = true
+	self._elem_by_idx = ft
+	self._idx_by_elem = rt
+	self._top_is_right = true
 end
 
 local function pop_common(self, on_top)
@@ -60,40 +60,40 @@ local function pop_common(self, on_top)
 
 	local idx = 0
 	if on_top then
-		idx = self.top
-		self.top = self.top - 1
+		idx = self._top
+		self._top = self._top - 1
 	else
-		idx = self.bot
-		self.bot = self.bot + 1
+		idx = self._bot
+		self._bot = self._bot + 1
 	end
-	local res = self.elem_by_idx[idx]
-	self.elem_by_idx[idx] = nil
+	local res = self._elem_by_idx[idx]
+	self._elem_by_idx[idx] = nil
 	self._len = self._len - 1
 	return res
 end
 
 function uq:pop()
-	return pop_common(self, self.top_is_right)
+	return pop_common(self, self._top_is_right)
 end
 
 function uq:pop_left()
-	return pop_common(self, not self.top_is_right)
+	return pop_common(self, not self._top_is_right)
 end
 
 function uq:remove(targ_item)
-	local targ_idx = self.idx_by_elem[targ_item]
+	local targ_idx = self._idx_by_elem[targ_item]
 	if not targ_idx then
 		return
 	end
-	for i = targ_idx,self.top do
-		self.elem_by_idx[i] = self.elem_by_idx[i+1]
+	for i = targ_idx,self._top do
+		self._elem_by_idx[i] = self._elem_by_idx[i+1]
 	end
 	self._len = self._len - 1
-	self.top = self.top - 1
+	self._top = self._top - 1
 end
 
 function uq:reverse()
-	self.top_is_right = not self.top_is_right
+	self._top_is_right = not self._top_is_right
 end
 
 function uq:rotate(count)
@@ -112,29 +112,29 @@ end
 local function traverser(self, top_to_bottom)
 	local start, inc = 0, 0
 	if top_to_bottom then
-		start = self.top
+		start = self._top
 		inc = -1
 	else
-		start = self.bot
+		start = self._bot
 		inc = 1
 	end
 
 	return function(self, prev)
 		local idx = start
-		local v = self.idx_by_elem[prev]
+		local v = self._idx_by_elem[prev]
 		if prev ~= nil then
 			idx = v + inc
 		end
-		return self.elem_by_idx[idx]
+		return self._elem_by_idx[idx]
 	end
 end
 
 function uq:right_to_left()
-	return traverser(self, self.top_is_right), self, nil
+	return traverser(self, self._top_is_right), self, nil
 end
 
 function uq:left_to_right()
-	return traverser(self, not self.top_is_right), self, nil
+	return traverser(self, not self._top_is_right), self, nil
 end
 
 function uq:len()
@@ -142,7 +142,7 @@ function uq:len()
 end
 
 function uq:contains(elem)
-	return self.idx_by_elem[elem] ~= nil
+	return self._idx_by_elem[elem] ~= nil
 end
 
 uq.pop_right = uq.pop
